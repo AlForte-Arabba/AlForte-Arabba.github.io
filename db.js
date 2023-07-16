7a049f34dbce768a3cd0bd696539629d60193c31
AlForte-Arabba <139552167+AlForte-Arabba@users.noreply.github.com>
2023-07-16 09:14:24 +0200

    Update db.js

--- db.js
+++ db.js
@@ -2,8 +2,14 @@ var data =
 {
 	"italiano":
 	{
-		"MENU TURISTICI": "MENU TURISTICI",
-		"ANTIPASTI": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non adipisci eligendi repellat ad dolor veritatis itaque sequi minus iste, doloremque. Officiis non eaque atque excepturi repudiandae nulla eos eligendi magni molestiae eius distinctio, voluptas pariatur incidunt et culpa inventore aspernatur recusandae nihil asperiores, vitae, maiores laborum quasi perspiciatis natus dignissimos! Accusantium aliquam nostrum impedit dignissimos iste, iure inventore! Sapiente, labore earum ut dicta ducimus asperiores laudantium natus officiis, quisquam placeat aspernatur voluptatum aut voluptates tenetur quos magni fugit quia. Fugiat."
+		"men": "MENU TURISTICI",
+		"ant": "ANTIPASTI",
+		"pri": "PRIMI",
+		"sec": "SECONDI",
+		"con": "CONTORNI",
+		"ben": "BENVENUTI",
+		"vin": "CARTA DEI VINI",
+		"all": "MENU ALLERGENI"
 	},
 	"english":
 	{
@@ -12,7 +18,7 @@ var data =
 		"pri": "FIRST COURSE",
 		"sec": "SECOND COURSE",
 		"con": "GARNISH",
-		"alt": "MORE INFO",
+		"ben": "WELCOME",
 		"vin": "WINE LIST",
 		"all": "ALLERGEN MENU"
 	},
@@ -23,7 +29,7 @@ var data =
 		"pri": "VORSPEISEN",
 		"sec": "HAUPTGERICHTE",
 		"con": "GARNISH",
-		"alt": "MEHR INFO"
+		"ben": "WILLKOMMEN"
 		"vin": "WEINKARTE",
 		"all": "ALLERGEN-MENÜ"
 	}
